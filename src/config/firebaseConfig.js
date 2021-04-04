6e3b47323d4981fbe6db730e06abbeb86c20a2dd
nhanbka <nhanletrong999@gmail.com>
2021-04-05 00:07:08 +0700

    [feat] add connect to firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDe_gk3OK3eGLIHuDtfI9fCmjRRNeeWxg8",
+    authDomain: "evernote-11311.firebaseapp.com",
+    databaseURL: "https://evernote-11311-default-rtdb.firebaseio.com",
+    projectId: "evernote-11311",
+    storageBucket: "evernote-11311.appspot.com",
+    messagingSenderId: "53396899609",
+    appId: "1:53396899609:web:858af4b9ec5d70c9bd6788",
+    measurementId: "G-6NGS313CZL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+export default firebaseConfig
--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore,  applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
+import thunk from 'redux-thunk';
+import {
+  ReactReduxFirebaseProvider, 
+  getFirebase
+} from 'react-redux-firebase';
+import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore';
+import rootReducer from './store/reducers/rootReducer';
+import firebaseConfig from './config/firebaseConfig';
 
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+),
+reduxFirestore(firebaseConfig)
+);
+const rrfProps = {
+  firebase,
+  config: firebaseConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps} >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
